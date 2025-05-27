cf9ae503df1a7355d7d93972d183824e46ef4575
chikeluba <jpokochike@gmail.com>
2025-05-27 23:47:11 +0100

    allow-all-hosts

--- backend/mybasicapp/mybasicapp/settings.py
+++ backend/mybasicapp/mybasicapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-dg9bq4k!giy)g7s-(cf0jsr%h01gs13+oemz#+717pezu5*s2c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
