3d9529e9d1818a77aa572bb31cde152981c687c9
chikeluba <jpokochike@gmail.com>
2025-09-21 11:01:51 +0100

    added-file-to-ignore-and-set-up-postgresql-database

--- backend/mybasicapp_graphql/mybasicapp_graphql/settings.py
+++ backend/mybasicapp_graphql/mybasicapp_graphql/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'mybasicapp_graphql.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
+DATABASE = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoappdb',
+        'USER': 'chikeluba',
+        'PASSWORD': 'treason',
+        'HOST': '192.168.25.128',
+        'PORT': 5432,
     }
 }
 
