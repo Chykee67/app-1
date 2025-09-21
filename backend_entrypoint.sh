79da9c77a75c085458e4a7aac86a50d2eaf199b4
chikeluba <jpokochike@gmail.com>
2025-09-21 12:13:33 +0100

    trying-database-settings-with-new-entrypoint-1

--- backend_entrypoint.sh
+++ backend_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-python /app/backend/mybasicapp_graphql/manage.py makemigrations account todo
+python3 /app/backend/mybasicapp_graphql/manage.py makemigrations account todo
 
-python /app/backend/mybasicapp_graphql/manage.py migrate
+python3 /app/backend/mybasicapp_graphql/manage.py migrate
 
 uwsgi /app/backend/djangoapp.ini
