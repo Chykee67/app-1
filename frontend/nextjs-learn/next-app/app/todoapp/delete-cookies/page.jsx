99f486cc3b19ebfe2278f92b8d93b99edb6efa02
chikeluba <jpokochike@gmail.com>
2025-10-01 15:27:29 +0100

    created-a-delete-cookie-page-2

--- frontend/nextjs-learn/next-app/app/todoapp/delete-cookies/page.jsx
+++ frontend/nextjs-learn/next-app/app/todoapp/delete-cookies/page.jsx
@@ -4,21 +4,14 @@ import { getClient } from '../../lib/ApolloClient'
 
 export default async function DeleteCookies(){
 
-    const handleClick = async(e) => {
-        e.preventDefault();
+    try {
+        const { data } = await getClient().mutate(
+            {mutation: DELETE_TOKEN_COOKIE}
+        )
 
-        try {
-            const { data } = await getClient().mutate(
-                {mutation: DELETE_TOKEN_COOKIE}
-            )
-        } catch(error){
-            console.error('deleting cookie error', error);
-        }
+        console.log(data)
+    } catch(error){
+        console.error('deleting cookie error', error);
     }
 
-    return (
-        <div>
-            <button onClick={handleClick}>Delete Cookie</button>
-        </div>
-    )
 }
